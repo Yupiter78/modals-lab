8c5e6cd7af5264e192ff46d7dd3dd9a158674b1c
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-01-22 03:55:04 +0300

    init: start project

---
+++ api/book.api.js
@@ -0,0 +1,127 @@
+const books = [
+    {
+        _id: "67rdca3eeb7f6fgeed471815",
+        title: "Властелин колец",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_fellowship_of_the_ring.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471816",
+        title: "Гарри Поттер и философский камень",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/harry_potter_and_the_sorcerer's_stone.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471817",
+        title: "Оно",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/it.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471818",
+        title: "Гордость и предубеждение",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/pride_and_prejudice.jpg?raw=true",
+    },
+
+    {
+        _id: "67rdca3eeb7f6fgeed471819",
+        title: "Приключения Шерлока Холмса",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_adventures_of_sherlock_holmes.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471820",
+        title: "Точка обмана",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/deception_point_dan_brown.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471821",
+        title: "1984",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/1984_george_orwell.png?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471822",
+        title: "Ромео и Джульетта",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/romeo_and_juliet.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471823",
+        title: "Игра престолов",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/a_game_of_thrones.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471824",
+        title: "Мастер и Маргарита",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_master_and_margarita.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471825",
+        title: "Война и мир",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/war_and_peace.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471826",
+        title: "Три товарища",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/three_comrades.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471827",
+        title: "Метро 2033",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/metro-2033.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471828",
+        title: "Психология масс",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_psychology_of_the_masses.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471829",
+        title: "Тень ветра",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_shadow_of_the_wind.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471830",
+        title: "Граф Монте-Кристо",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_count_of_monte_cristo.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471831",
+        title: "Маленький принц",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_little_prince.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471832",
+        title: "Возвращение Мюнхгаузена",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_return_of_munchausen.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471833",
+        title: "Приключения Чиполлино",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_adventures_of_chipollino.jpg?raw=true",
+
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed471834",
+        title: "Код Да Винчи",
+        img: "https://github.com/Yupiter78/bookstore/blob/main/src/app/assets/the_da_vinci_code.jpg?raw=true",
+
+    }
+];
+
+export default books;
+
+
+
---
+++ index.js
@@ -0,0 +1,41 @@
+import content from "./api/book.api.js";
+
+const customBox = document.querySelector("#custom_box");
+const modal = document.querySelector('.custom_modal');
+const modalContent = document.querySelector('.custom_modal-content');
+const closeBtn = document.querySelector('.close');
+
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none'; // Скрытие модального окна при клике вне изображения
+    }
+});
+
+closeBtn.addEventListener('click', function () {
+    modal.style.display = 'none'; // Скрытие модального окна при клике на кнопку закрытия
+});
+
+window.handleOpenModal = function (src) {
+    modalContent.src = src;
+    modal.style.display = "block";
+}
+
+
+if (Array.isArray(content)) {
+    const galleryItemTemplates = content.map((item) => {
+        const titleContent = item.title ? `<p class="card-text">${item.title}</p>` : "";
+        return `<div class="col">
+      <div class="card shadow-sm" style="width: 80%;">
+        <img src=${item.img} width="100%" height="100%" alt="image" class="modal-trigger"/>
+        <div class="card-body">${titleContent}
+          <div class="d-flex justify-content-center align-items-center">
+              <button type="button" class="btn btn-sm btn-primary w-75" data-img="${item.img}" onclick="handleOpenModal(this.getAttribute('data-img'))">Просмотр</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    });
+
+    customBox.insertAdjacentHTML("beforeend", galleryItemTemplates.join(""));
+}
+
